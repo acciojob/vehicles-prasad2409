e8b51084502cc2d1516816737d283ea25a934524
prasad2409 <durgaprasad24a@gmail.com>
2023-02-11 17:03:06 +0530

    Solution 1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -6,20 +6,57 @@ public class Car extends Vehicle {
     private int doors;
     private int gears;
     private boolean isManual;
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
     private int currentGear;
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
     private int seats;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        setName(name);
+        this.wheels = wheels;
+        this.type = type;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
+        this.seats = seats;
     }
 
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        super.move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+    public int getCurrentDirection(){
+        return super.getCurrentDirection();
+    }
+
+    public int getCurrentSpeed() {
+        return super.getCurrentSpeed();
+    }
+    public void setCurrentSpeed(int currentSpeed) {
+        super.setCurrentSpeed(currentSpeed);
+    }
+    public void setCurrentDirection(int currentDirection) {
+        super.setCurrentDirection(currentDirection);
+    }
+    public void setName(String name){
+        super.setName(name);
+    }
+    public void stop(){
+        super.stop();
+    }
+    public void move(int speed, int direction){
+        setCurrentSpeed(speed);
+        setCurrentDirection(direction);
     }
 }
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,18 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super.setName(name);
+        super.setManual(isManual);
         //Use arbitrary values for parameters which are not mentioned
     }
-
+    public int getCurrentDirection(){
+        return super.getCurrentDirection();
+    }
+    public int getCurrentSpeed(){
+        return super.getCurrentSpeed();
+    }
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,12 +26,24 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            super.stop();
+            super.setCurrentGear(1);
             //Stop the car, set gear as 1
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+           super.changeSpeed(newSpeed, getCurrentDirection());
+           if(newSpeed >=1 && newSpeed <= 50) super.setCurrentGear(1);
+           else if(newSpeed>=51 && newSpeed <= 100) super.setCurrentGear(2);
+           else if(newSpeed >= 101 && newSpeed <=150) super.setCurrentGear(3);
+           else if(newSpeed >= 151 && newSpeed <= 200) super.setCurrentGear(4);
+           else if(newSpeed >= 201 && newSpeed <= 250) super.setCurrentGear(5);
+           else super.setCurrentGear(6);
         }
+    }
+    public void move(int speed,int direction){
+        super.setCurrentSpeed(speed);
+        super.setCurrentDirection(direction);
     }
 }
